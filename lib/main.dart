78b9c0eec54550a968d1258d3d6d33a111f1b0d2
I Komang Sena Aji Buwana <ikomangsena@yahoo.co.id>
2024-07-06 19:35:50 +0700

    chore: initialize firebase and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -12,6 +14,9 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
