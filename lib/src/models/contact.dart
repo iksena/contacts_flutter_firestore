e73ff0dcbaded40b8a220f012e9982974a35c3c1
I Komang Sena Aji Buwana <ikomangsena@yahoo.co.id>
2024-07-08 22:05:03 +0700

    feat: Refactor contact model

--- lib/src/models/contact.dart
+++ lib/src/models/contact.dart
@@ -1,8 +1,32 @@
 class Contact {
-  const Contact(this.id, this.name, this.email, this.address);
-
-  final int id;
+  final String id;
   final String name;
+  final String phone;
   final String email;
   final String address;
+
+  const Contact({
+    required this.id, 
+    required this.name, 
+    required this.phone,
+    required this.email,
+    required this.address
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'address': address,
+    };
+  }
+
+  Contact.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        name = map['name'],
+        phone = map['phone'],
+        email = map['email'],
+        address = map['address'];
 }
