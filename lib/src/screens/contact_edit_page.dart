b77b21bf6971811452c164b7785f9c919e502ac3
I Komang Sena Aji Buwana <ikomangsena@yahoo.co.id>
2024-07-09 22:21:02 +0700

    feat: add form validations

--- lib/src/screens/contact_edit_page.dart
+++ lib/src/screens/contact_edit_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_contacts/src/models/contact.dart';
 import 'package:get/get.dart';
 
 class EditContactPage extends StatefulWidget {
+  const EditContactPage({super.key});
+
   @override
   _EditContactPageState createState() => _EditContactPageState();
 }
@@ -26,13 +28,82 @@ class _EditContactPageState extends State<EditContactPage> {
     _address = contact?.address ?? '';
   }
 
+  String? nameValidator(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a name';
+    }
+    if (value.length <= 3 || value.length > 100) {
+      return 'Name should be more than 3 chars and under 100 chars';
+    }
+    return null;
+  }
+
+  String? emailValidator(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email';
+    }
+
+    final emailRegex =
+        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegex.hasMatch(value) || value.length > 300) {
+      return 'Please enter a valid email';
+    }
+
+    return null;
+  }
+
+  String? phoneValidator(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a phone number';
+    }
+
+    final phoneRegex = RegExp(r'^\+?[0-9]{7,15}$');
+    if (!phoneRegex.hasMatch(value)) {
+      return 'Please enter a valid phone number';
+    }
+
+    return null;
+  }
+
+  String? addressValidator(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an address';
+    }
+    if (value.length <= 3 || value.length > 500) {
+      return 'Address should be more than 3 chars and under 500 chars';
+    }
+    return null;
+  }
+
+  void Function() onPressedSubmit(contactsController) {
+    return () {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        final savedContact = Contact(
+            id: contact?.id ?? DateTime.now().toString(),
+            name: _name,
+            phone: _phone,
+            email: _email,
+            address: _address);
+
+        if (contact == null) {
+          contactsController.addContact(savedContact);
+        } else {
+          contactsController.updateContact(savedContact);
+        }
+
+        Get.back();
+      }
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     final contactsController = Get.find<ContactsController>();
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(contact == null ? 'Add Contact' : 'Edit Contact'),
+        title: Text(contact == null ? 'New Contact' : 'Edit Contact'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -43,70 +114,31 @@ class _EditContactPageState extends State<EditContactPage> {
               TextFormField(
                 initialValue: _name,
                 decoration: const InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a name';
-                  }
-                  return null;
-                },
+                validator: nameValidator,
                 onSaved: (value) => _name = value!,
               ),
               TextFormField(
                 initialValue: _phone,
                 decoration: const InputDecoration(labelText: 'Phone'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a phone number';
-                  }
-                  return null;
-                },
+                validator: phoneValidator,
                 onSaved: (value) => _phone = value!,
               ),
               TextFormField(
                 initialValue: _email,
                 decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
+                validator: emailValidator,
                 onSaved: (value) => _email = value!,
               ),
               TextFormField(
                 initialValue: _address,
                 decoration: const InputDecoration(labelText: 'Address'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an address';
-                  }
-                  return null;
-                },
+                validator: addressValidator,
                 onSaved: (value) => _address = value!,
               ),
               const SizedBox(height: 20),
               ElevatedButton(
+                onPressed: onPressedSubmit(contactsController),
                 child: Text(contact == null ? 'Add' : 'Update'),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    final savedContact = Contact(
-                      id: contact?.id ?? DateTime.now().toString(),
-                      name: _name,
-                      phone: _phone,
-                      email: _email,
-                      address: _address
-                    );
-
-                    if (contact == null) {
-                      contactsController.addContact(savedContact);
-                    } else {
-                      contactsController.updateContact(savedContact);
-                    }
-
-                    Get.back();
-                  }
-                },
               ),
             ],
           ),
